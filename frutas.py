d112169e7f0e7a7d49e207d6be89dcdfdcc49998
FernandoGarciaRangel <fernandolyon.fg@gmail.com>
2026-03-05 20:23:01 -0300

    Iniciando

--- frutas.py
+++ frutas.py
@@ -1,6 +1,22 @@
-#%%
+# %%
 
 import pandas as pd
 
 df = pd.read_excel("data/dados_frutas.xlsx")
+df.head()
 
+#%%
+
+from sklearn import tree
+
+arvore = tree.DecisionTreeClassifier()
+
+
+# %%
+y = df['Fruta']
+
+caracteristicas = ['Arredondada', 'Suculenta', 'Vermelha', 'Doce']
+x = df[caracteristicas]
+
+x
+# %%
