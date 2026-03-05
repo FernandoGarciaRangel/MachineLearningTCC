03d76e705927a4cf1605d9241de5df226f447a8e
FernandoGarciaRangel <fernandolyon.fg@gmail.com>
2026-03-05 20:39:56 -0300

    Fim aula 1

--- frutas.py
+++ frutas.py
@@ -9,7 +9,7 @@ df.head()
 
 from sklearn import tree
 
-arvore = tree.DecisionTreeClassifier()
+arvore = tree.DecisionTreeClassifier(random_state=42)
 
 
 # %%
@@ -19,4 +19,19 @@ caracteristicas = ['Arredondada', 'Suculenta', 'Vermelha', 'Doce']
 x = df[caracteristicas]
 
 x
+# %%
+arvore.fit(x, y)
+# %%
+
+arvore.predict([[0, 0, 0, 0]])
+# %%
+import matplotlib.pyplot as plt
+
+plt.figure(dpi=400)
+
+tree.plot_tree(arvore, feature_names=caracteristicas, class_names=arvore.classes_, filled=True)
+# %%
+proba = arvore.predict_proba([[0, 0, 0, 0]])[0]
+
+pd.Series(proba, index=arvore.classes_)
 # %%
